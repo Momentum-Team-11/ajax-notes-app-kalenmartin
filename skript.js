fdff740849ba6e2ac7e5e7ac1af706b3fe9bbd9a
Kalen Martin <kalen.martin1@gmail.com>
2022-02-17 10:26:28 -0500

    miracles can happen. it works.glory

--- skript.js
+++ skript.js
@@ -1,160 +1,160 @@
 /* Must have -- MVP (minimum viable product) */
 
-const url = 'http://localhost:3000/notes'
-const noteList = document.getElementById('note-list')
-const noteForm = document.getElementById('note-form')
+// const url = 'http://localhost:3000/notes'
+// const noteList = document.getElementById('note-list')
+// const noteForm = document.getElementById('note-form')
 
-console.log('hello')
+// console.log('hello')
 
-// // event listener
-// form.addEventListener('submit', function (event) {
-//     event.preventDefault()
+// // // event listener
+// // form.addEventListener('submit', function (event) {
+// //     event.preventDefault()
 
-// const noteText = document.querySelector('#text').value
-//     createNote(noteText)
+// // const noteText = document.querySelector('#text').value
+// //     createNote(noteText)
+// // })
+
+
+// // Different listener events on list items //
+// // -- EVENT DELEGATION -- //
+// noteList.addEventListener('click', function (event) {
+// if (event.target.classList.contains('delete')) {
+//     deleteNote(event.target)
+// }
+// if (event.target.classList.contains('edit')) {
+//     editNote(event.target)
+// }
+// if (event.target.classList.contains('update-note')) {
+//     updateNote(event.target)
+// }
+// if (event.target.classList.contains('cancel')) {
+//     hideEditControls(event.target.parentElement)
+// }
 // })
 
-
-// Different listener events on list items //
-// -- EVENT DELEGATION -- //
-noteList.addEventListener('click', function (event) {
-if (event.target.classList.contains('delete')) {
-    deleteNote(event.target)
-}
-if (event.target.classList.contains('edit')) {
-    editNote(event.target)
-}
-if (event.target.classList.contains('update-note')) {
-    updateNote(event.target)
-}
-if (event.target.classList.contains('cancel')) {
-    hideEditControls(event.target.parentElement)
-}
-})
-
-// GET //
-function dezNotes() {
-    fetch(url)
-        .then((res) => res.json())
-        .then((data) => {
-        console.log(data)
-    for(let noteObj of data) {
-        renderNoteItem(noteObj)
-    }
-    })
-}
+// // GET //
+// function dezNotes() {
+//     fetch(url)
+//         .then((res) => res.json())
+//         .then((data) => {
+//         console.log(data)
+//     for(let noteObj of data) {
+//         renderNoteItem(noteObj)
+//     }
+//     })
+// }
 
 
 
-//CRUD //
+// //CRUD //
 
 
-//DELETE
-function deleteNote(element) {
+// //DELETE
+// function deleteNote(element) {
 
-const noteId = element.parentElement.id
-fetch(`http://localhost:3000/notes/${noteId}`, {
-    method: 'DELETE',
-}).then(function () {
-    element.parentElement.remove()
-})
-}
+// const noteId = element.parentElement.id
+// fetch(`http://localhost:3000/notes/${noteId}`, {
+//     method: 'DELETE',
+// }).then(function () {
+//     element.parentElement.remove()
+// })
+// }
 
-function updateNote(element) {
-const noteId = element.parentElement.id
-const noteBody = document.querySelector('note-body')
-fetch(`http://localhost:3000/notes/${noteId}`, {
-    method: 'PATCH',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-    item: noteBody.value,
-    updated_at: moment().format(),
-    }),
-})
-    .then(function (res) {
-    return res.json()
-    })
-    .then(function (data) {
-    console.log(noteBody)
-    renderNoteText(element.parentElement, data)
-    })
-}
+// function updateNote(element) {
+// const noteId = element.parentElement.id
+// const noteBody = document.querySelector('note-body')
+// fetch(`http://localhost:3000/notes/${noteId}`, {
+//     method: 'PATCH',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({
+//     item: noteBody.value,
+//     updated_at: moment().format(),
+//     }),
+// })
+//     .then(function (res) {
+//     return res.json()
+//     })
+//     .then(function (data) {
+//     console.log(noteBody)
+//     renderNoteText(element.parentElement, data)
+//     })
+// }
 
-// event listener //
-//the submit event listener to the POST//
-// Post //
+// // event listener //
+// //the submit event listener to the POST//
+// // Post //
 
-noteForm.addEventListener('submit', function (event) {
-    event.preventDefault()
-function createNote(myNotes) {
-fetch(url, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json'},
-    body:JSON.stringify({
-    item: myNotes
-    // created_at: moment().format(),
-    }),
-})
-.then((res) => res.json())
-.then((data) => {
-    renderNoteItem(data)
-    noteForm.reset();
-    });
-}});
+// noteForm.addEventListener('submit', function (event) {
+//     event.preventDefault()
+// function createNote(myNotes) {
+// fetch(url, {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json'},
+//     body:JSON.stringify({
+//     item: myNotes
+//     // created_at: moment().format(),
+//     }),
+// })
+// .then((res) => res.json())
+// .then((data) => {
+//     renderNoteItem(data)
+//     noteForm.reset();
+//     });
+// }});
 
-/***** DOM changing functions *****/
+// /***** DOM changing functions *****/
 
-function renderNoteItem(noteObj) {
-const itemEl = document.createElement('li')
-itemEl.id = noteObj.id
-itemEl.classList.add(
-    'lh-copy',
-    'pv3',
-    'ba',
-    'bl-0',
-    'bt-0',
-    'br-0',
-    'b--dotted',
-    'b--black-3'
-)
-renderNoteText(itemEl, noteObj)
-noteList.prepend(itemEl)
-}
+// function renderNoteItem(noteObj) {
+// const itemEl = document.createElement('li')
+// itemEl.id = noteObj.id
+// itemEl.classList.add(
+//     'lh-copy',
+//     'pv3',
+//     'ba',
+//     'bl-0',
+//     'bt-0',
+//     'br-0',
+//     'b--dotted',
+//     'b--black-3'
+// )
+// renderNoteText(itemEl, noteObj)
+// noteList.prepend(itemEl)
+// }
 
-function renderNoteText(noteListItem, noteObj) {
-noteListItem.innerHTML = `<span class="dib w-60">${noteObj.item}</span><i class="ml2 dark-red fas fa-times delete"></i><i class="ml3 fas fa-edit edit"></i>`
-}
+// function renderNoteText(noteListItem, noteObj) {
+// noteListItem.innerHTML = `<span class="dib w-60">${noteObj.item}</span><i class="ml2 dark-red fas fa-times delete"></i><i class="ml3 fas fa-edit edit"></i>`
+// }
 
-function editNote(element) {
-showEditInput(element.parentElement)
-}
+// function editNote(element) {
+// showEditInput(element.parentElement)
+// }
 
-function showEditInput(noteItem) {
-noteItem.innerHTML = `
-    <input class="edit-text" type="text" value="${noteItem.textContent}"focus>
-    <textarea class="update-note" data-note=${noteBody.textarea.id}>save</button>
-    <button class="submit">submit</button>
-`
-noteItem.querySelector('input').select()
-}
+// function showEditInput(noteItem) {
+// noteItem.innerHTML = `
+//     <input class="edit-text" type="text" value="${noteItem.textContent}"focus>
+//     <textarea class="update-note" data-note=${noteBody.textarea.id}>save</button>
+//     <button class="submit">submit</button>
+// `
+// noteItem.querySelector('input').select()
+// }
 
-function hideEditControls(noteItem) {
-fetch(`http://localhost:3000/notes/${noteItem.id}`)
-    .then((res) => res.json())
-    .then((data) => {
-    console.log(data)
-    renderNoteText(noteItem, data)
-    })
-}
+// function hideEditControls(noteItem) {
+// fetch(`http://localhost:3000/notes/${noteItem.id}`)
+//     .then((res) => res.json())
+//     .then((data) => {
+//     console.log(data)
+//     renderNoteText(noteItem, data)
+//     })
+// }
 
-function clearInputs() {
-form.reset()
-}
+// function clearInputs() {
+// form.reset()
+// }
 
 /**** Function that runs as soon as the script file loads *****/
 // call this when the script first runs (on page load)
 // This runs only on the first load!
-dezNotes()
+// dezNotes()
 
 
 
@@ -166,3 +166,165 @@ dezNotes()
 // a way to mark as completed
 // indicate whether note has been completed or not
 // filter completed/not completed
+
+
+
+
+
+
+
+const url = 'http://localhost:3000/notes'
+const noteList = document.getElementById('note-list')
+const noteForm = document.getElementById('note-form')
+const subButton = document.getElementById('submit-button')
+const saveButton = document.getElementById('save-button')
+const cancelButton = document.getElementById('cancel-button')
+let noteId
+let date
+
+function deezNotes() {
+    document.getElementById("note-list").innerHTML = ''
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+        // take all the notes
+        // loop through and create a new note item on the page for each one
+            for (let noteObj of data) {
+                renderNote(noteObj)
+            }
+    })
+}
+
+function renderNote(noteObj) {
+    const itemEl = document.createElement('li')
+    itemEl.id = noteObj.id
+    itemEl.classList.add('lh-copy','pv3','ba','bl-0','bt-0','br-0','b--dotted','b--black-3')
+    itemEl.innerHTML = `<span>${noteObj.title}</span><i alt="delete note" class="ml2 dark-red fas fa-times delete"></i><i alt="edit note" class="ml3 fas fa-edit edit"></i><br><p>${noteObj.date}</p>`
+    noteList.prepend(itemEl)
+}
+
+
+//List notes at top of page
+deezNotes()
+
+//Make delete and edit icons clicky
+noteList.addEventListener('click', function (event) {
+    event.preventDefault()
+    if (event.target.classList.contains('delete')) {
+        deleteNote(event.target)
+    }
+    if (event.target.classList.contains('edit'))
+        editMode(event.target)
+})
+
+
+//make submit button clicky
+subButton.addEventListener('click', function (event) {
+    event.preventDefault()
+    console.log("submit")
+    
+    //If either field is empty, don't submit the note
+    if (document.getElementById("newNote").value === '' || document.getElementById("currentBody").value === '') {
+        return
+    } else {
+    createNote(event)
+    }
+})
+
+//make edit button clicky
+saveButton.addEventListener('click', function (event) {
+    event.preventDefault()
+    editNote(event)
+})
+
+//make cancel button clicky
+cancelButton.addEventListener('click', function (event) {
+    event.preventDefault()
+    console.log("cancel")
+    document.getElementById("note-edit").innerText = "New Note"
+    const newNote = document.querySelector('#newNote')
+    const noteText = document.querySelector('#currentBody')
+    newNote.value = ''
+    noteText.value = ''
+    button1.style = "display: block;"
+    button2.style = "display: none;"
+    button3.style = "display: none;"
+})
+
+
+
+function createNote() {
+    const newNote = document.querySelector('#newNote')
+    const noteText = document.querySelector('#currentBody')
+    date = Date()
+    fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            title: newNote.value,
+            body: noteText.value,
+            // date: date,
+        }),
+    })
+        .then((res) => res.json())
+        .then((data) => {
+    renderNote(data)
+    })
+    newNote.value = ''
+    noteText.value = ''
+}
+
+
+
+function deleteNote(element) {
+    const noteId = element.parentElement.id
+    fetch(`http://localhost:3000/notes/${noteId}`, {
+        method: 'DELETE',
+    }).then(function() {
+        element.parentElement.remove()
+    })
+}
+
+
+
+function editMode(element) {
+    noteId = element.parentElement.id
+    subButton.style = "display: none;"
+    saveButton.style = "display: row;"
+    cancelButton.style = "display: row;"
+    //replace current form with edit form
+    document.getElementById("note-edit").innerText = "Edit Note"
+
+    fetch(`http://localhost:3000/notes/${noteId}`)
+    .then((res) => res.json())
+    .then((data) => {
+        const newNote = document.getElementById("newNote")
+        const currentBody = document.getElementById("currentBody")
+        newNote.value = data.title
+        currentBody.value = data.body
+        })
+}
+
+
+
+function editNote() {
+    const newNote = document.querySelector('#newNote')
+    const noteText = document.querySelector('#currentBody')
+    // date = Date()
+    document.getElementById(`${noteId}`).innerHTML = `<span class="dib w-60">${newNote.value}</span><i alt="delete note" class="ml2 dark-red fas fa-times delete"></i><i alt="edit note" class="ml3 fas fa-edit edit"></i><br><p>${date}</p>`
+    fetch(`http://localhost:3000/notes/${noteId}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            title: newNote.value,
+            body: noteText.value,
+            // updated_at: moment().format(),
+        })
+    })
+    document.getElementById("note-edit").innerText = "Plzdontforgetthis"
+    newNote.value = ''
+    noteText.value = ''
+    subButton.style = "display: block;"
+    saveButton.style = "display: none;"
+    cancelButton.style = "display: none;"
+}
